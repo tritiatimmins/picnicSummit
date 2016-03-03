931c7d233db25f7fefe271fc58cddb603a253f62
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-03-03 15:50:50 -0800

    Should show file structure

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ var mongoose 	= require('mongoose');
 // Configuration to database
 // var db = require('./config/db.js')
 
+mongoose.connect('mongodb://localhost/shortly'); // connect to mongo database named shortly
+
 //set our port
 var port = process.env.PORT || 8000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use(express.bodyParser())
+// app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
 //app routes go here ========================
-
+// require("./app/models")(app, express)
 
 //start server ==============================
 app.listen(port);
